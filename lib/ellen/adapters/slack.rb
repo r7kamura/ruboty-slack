93e1db32d50868cd69c04a7f4117e23c04ad031a
Ryo Nakamura <r7kamura@gmail.com>
2014-04-22 23:54:23 +0900

    Support multiline message

--- lib/ellen/adapters/slack.rb
+++ lib/ellen/adapters/slack.rb
@@ -4,6 +4,8 @@ require "yaml"
 module Ellen
   module Adapters
     class Slack < Base
+      INTERVAL = 0.1
+
       env :SLACK_CHANNEL, "Channel name the bot logs in at first"
       env :SLACK_PASSWORD, "Account's IRC password (See https://my.slack.com/account/gateways)"
       env :SLACK_TEAM, "Account's team name"
@@ -16,7 +18,10 @@ module Ellen
       end
 
       def say(body, options = {})
-        client.privmsg(channel, ":#{body}")
+        body.split("\n").each do |line|
+          client.privmsg(channel, ":#{line}")
+          wait
+        end
       end
 
       private
@@ -69,6 +74,13 @@ module Ellen
 
       def connect
         client.run!
+      end
+
+      # Wait for Slack IRC Gateway to process message queue.
+      # If we send 2 messages in too short time,
+      # sometimes the slack double-renders the first message by mistake.
+      def wait
+        sleep(INTERVAL)
       end
     end
   end
